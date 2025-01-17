ad094e1968bf409c2d9bee30369078b0a52aa456
Ivan Orekhov <ivanorekhov04022005@gmail.com>
2025-01-17 19:13:50 +0300

    memory-game

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import initialCards from "./initialCards";
+import Board from "./components/board";
+import GameInfo from "./components/gameInfo";
+import './app.css'
+
+function App() {
+  const [cards, setCards] = useState(initialCards);
+  const [flippedCards, setFlippedCards] = useState([]);
+  const [matchedCards, setMatchedCards] = useState([]);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    setCards(shuffleCards(initialCards));
+  }, []);
+
+  useEffect(() => {
+    if (flippedCards.length === 2) {
+      setIsDisabled(true);
+      const [first, second] = flippedCards;
+      if (cards[first].image === cards[second].image) {
+        setMatchedCards([...matchedCards, first, second]);
+        setFlippedCards([]);
+        setIsDisabled(false);
+      } else {
+        setTimeout(() => {
+          const newCards = cards.map((card, index) => {
+            if (index === first || index === second) {
+              card.isFlipped = false;
+            }
+            return card;
+          });
+          setCards(newCards);
+          setFlippedCards([]);
+          setIsDisabled(false);
+        }, 1000);
+      }
+    }
+  }, [cards, flippedCards, matchedCards]);
+
+  const hadleCardClick = (index) => {
+    if (
+      isDisabled ||
+      flippedCards.includes(index) ||
+      matchedCards.includes(index)
+    )
+      return;
+    const newCards = cards.map((card, i) => {
+      if (i === index) {
+        card.isFlipped = true;
+      }
+      return card;
+    });
+    setCards(newCards);
+    setFlippedCards([...flippedCards, index]);
+  };
+
+  const resetGame = () => {
+    const resetCards = initialCards.map((card) => ({
+      ...card,
+      isFlipped: false,
+    }));
+    setCards(shuffleCards(resetCards));
+    setFlippedCards([]);
+    setMatchedCards([]);
+    setIsDisabled(false);
+  };
+
+  const shuffleCards = (cards) => {
+    return cards.sort(() => Math.random() - 0.5);
+  };
+  return (
+    <div className="app">
+      <h1>Memory Game</h1>
+      <Board cards={cards} onCardClick={hadleCardClick} />
+      <GameInfo matchedCards={matchedCards} onReset={resetGame} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/initialCards.js
@@ -0,0 +1,30 @@
+import dog from './images/dog.png'
+import cat from './images/cat.png'
+import parrot from './images/parrot.png'
+import beaver from './images/beaver.png'
+import bear from './images/bear.png'
+import bunny from './images/bunny.png'
+import owl from './images/owl.png'
+import raccoon from './images/raccoon.png'
+
+
+const initialCards = [
+    {id: 1, image: dog, isFlipped: false},
+    {id: 2, image: dog, isFlipped: false},
+    {id: 3, image: cat, isFlipped: false},
+    {id: 4, image: cat, isFlipped: false},
+    {id: 5, image: parrot, isFlipped: false},
+    {id: 6, image: parrot, isFlipped: false},
+    {id: 7, image: beaver, isFlipped: false},
+    {id: 8, image: beaver, isFlipped: false},
+    {id: 9, image: bear, isFlipped: false},
+    {id: 10, image: bear, isFlipped: false},
+    {id: 11, image: bunny, isFlipped: false},
+    {id: 12, image: bunny, isFlipped: false},
+    {id: 13, image: owl, isFlipped: false},
+    {id: 14, image: owl, isFlipped: false},
+    {id: 15, image: raccoon, isFlipped: false},
+    {id: 16, image: raccoon, isFlipped: false}
+]
+
+export default initialCards
